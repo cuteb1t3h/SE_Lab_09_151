6710897200edbcca53edf79ed8540e5a01161baf
cuteb1t3h <cuteb1t3h@gmail.com>
2023-02-21 09:36:00 +0300

    "L-02: master 2"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, " + args[0]);
+
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextFloat();
+        double y = scanner.nextFloat();
+
+        double result = (3 * Math.pow(Math.cos(x - (Math.PI/6)), 2)/(0.5 + Math.pow(Math.sin(y),2)));
+
+        System.out.println(result);
     }
 }
