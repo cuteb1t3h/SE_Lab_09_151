f059ba4af752668a587164f80f7b7107d9d2c58c
cuteb1t3h <cuteb1t3h@gmail.com>
2023-02-28 01:12:41 +0300

    "L-02: hello_chat2"

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,5 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -8,27 +7,35 @@ public class Client {
     private static final String SERVER_ADDRESS = "localhost";
     private static final int SERVER_PORT = 34522;
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
         // Создаем сокет для подключения к серверу
         Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
 
         // Получаем входной и выходной потоки для общения с сервером
         DataInputStream input = new DataInputStream(socket.getInputStream());
-        DataOutputStream output  = new DataOutputStream(socket.getOutputStream());
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
         // Вводим имя в консоли
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите имя: ");
+        System.out.println("Введите имя: ");
         String username = scanner.nextLine();
 
-        // Отправляем имя пользователя на сервер
+        // Отправляем имя пользователя на сервер, выводим ответ от сервера на консоль
         output.writeUTF(username);
+        System.out.println(input.readUTF());
 
-        // Читаем ответ от сервера и выводим его на консоль
-        String response = input.readUTF();
-        System.out.println(response);
+        String flag;
+        do {
+//            System.out.println("Введите сообщение: ");
+            flag = scanner.nextLine();
+            output.writeUTF(flag);
 
+            String message = input.readUTF();
+            System.out.println("Ответ сервера: " + message);
+        }while (!flag.equals("bye"));
+
+        output.close();
+        input.close();
         socket.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,33 +1,52 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     private static final int PORT = 34522;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         ServerSocket serverSocket = new ServerSocket(PORT);
         System.out.println("Сервер запущен и готов к работе...");
 
         while (true) {
             // Ожидаем подключения клиента
-            Socket socket = serverSocket.accept();
+            Socket clientSocket = serverSocket.accept();
 
             // Получаем входной и выходной потоки для общения с клиентом
-            DataInputStream input = new DataInputStream(socket.getInputStream());
-            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            DataInputStream input = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
 
             // Ждем от клиента сообщения с именем пользователя
-            String username = input.readUTF();;
+            String username = input.readUTF();
             System.out.println("К нам пришел " + username);
-
-            // Выводим клиенту приветственное сообщение
             output.writeUTF("Hello, " + username);
 
-            socket.close();
+            String flag;
+            int count = 0;
+            while ((flag = input.readUTF()) != null){
+                if (flag.equals("bye")){
+                    break;
+                }
+                count++;
+                String message = count + " - " + flag;
+                System.out.println("Получено сообщение №" + message);
+                output.writeUTF(message);
+            }
+
+            output.close();
+            input.close();
+            clientSocket.close();
+            serverSocket.close();
+
+            // Ждем от клиента сообщения с именем пользователя
+//            String username = input.readUTF();
+//            System.out.println("К нам пришел " + username);
+
+            // Выводим клиенту приветственное сообщение
+//            output.writeUTF("Hello, " + username);
         }
     }
 }
